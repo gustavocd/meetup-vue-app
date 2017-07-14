d722ac60cfd2373d83c25b7e3f10f3d281983204
gustavocd <gustavocastillo1993@hotmail.com>
2017-07-14 17:21:45 -0500

    Setup VueJS cli and Vuetify

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-title>My Title</v-toolbar-title>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
